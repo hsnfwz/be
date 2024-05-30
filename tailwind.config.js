edb1304ea9b8266e6a2364e3c9fd98da2dd0c431
Hussein Fawaz <husseinfawaz@MacBook-Pro-7.local>
2024-05-30 00:03:01 -0700

    done

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'playfair-display-regular': 'Playfair Display Regular',
+      'playfair-display-bold': 'Playfair Display Bold',
+      'playfair-display-italic': 'Playfair Display Italic',
+      'playfair-display-bold-italic': 'Playfair Display Bold Italic',
+      'montserrat-regular': 'Montserrat Regular',
+      'montserrat-bold': 'Montserrat Bold',
+      'montserrat-italic': 'Montserrat Italic',
+      'montserrat-bold-italic': 'Montserrat Bold Italic',
+    }
+  },
+  plugins: [],
+}
